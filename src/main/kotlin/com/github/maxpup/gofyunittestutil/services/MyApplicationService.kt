5a15ffcd039163a1fa88b6435a9f5611b35c6333
GitHub Action <action@github.com>
2022-04-17 04:49:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/maxpup/gofyunittestutil/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/maxpup/gofyunittestutil/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.maxpup.gofyunittestutil.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maxpup.gofyunittestutil.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/maxpup/gofyunittestutil/services/MyProjectService.kt
+++ src/main/kotlin/com/github/maxpup/gofyunittestutil/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.maxpup.gofyunittestutil.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maxpup.gofyunittestutil.MyBundle
 
 class MyProjectService(project: Project) {
 
